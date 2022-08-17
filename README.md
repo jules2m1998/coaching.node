0d703f815ae44acfe71dcccd024f8515df7d71b3
jules2m1998 <mevaajules9@gmail.com>
2022-08-17 21:53:02 +0100

    Hello word

---
+++ README.md
@@ -0,0 +1,21 @@
+# Formation node js coaching
+
+## Pour initialiser un projet node
+
+``` console
+npm init -y
+```
+
+* **npm** est le gestionnaire de packet pour node js il permet d'installer des librairies developpées par la communauté nodejs.
+* **init** est un attribut pour npm permettant d'initialiser un projet node
+* **-y** est un drapeau de l'attibut init permettant d'initialiser un projet en répondant oui à toutes les questions posée en console.
+
+
+## Pour créer un serveur nodejs
+
+``` console
+npm run dev
+```
+
+* **run** attribut de npm permettant l'exécution d'un script dans le fichier ***package.json***.
+* **dev** le nom du script à exécuter.
--- index.js
+++ index.js
@@ -1,14 +1,27 @@
+// Importation du packet permettant la création d'un serveur http
 const http = require('http');
 
+// Déclaration de l'hote qui permettra l'acces au serveur
 const hostname = '127.0.0.1';
+
+// Déclaration du port qui permettra l'acces au serveur
 const port = 3000;
 
+// Creation du serveur http
+/**
+ * @param req information sur la requête
+ * @param res information sur la reponse
+ */
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+    // Status de la réponse
+    res.statusCode = 200;
+    // type de contenu envoyé dans la réponse
+    res.setHeader('Content-Type', 'text/plain');
+    // Envoie de la réponse
+    res.end('Hello World');
 });
 
+// Démarrage du serveur sur le port et le nom d'hote défini
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
